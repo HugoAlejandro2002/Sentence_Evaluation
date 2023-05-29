471ff23921841503479fb060410ae2201d2cd18d
lolex <apazahuaychohugoalejandro@gmail.com>
2023-05-29 07:49:58 -0400

    fix evluation content

--- src/components/Evaluation.jsx
+++ src/components/Evaluation.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export const Evaluation = (props) => {
   const { evaluation } = props;
-  const { NotaGeneral,Caracteristicas } = evaluation;
-  const { Consejo, Puntuacion } = NotaGeneral;
+  const { ResumenGeneral,Caracteristicas } = evaluation;
+
+  const promedio = Caracteristicas.reduce((total, caracteristica) => total+caracteristica.Nota,0 )/Caracteristicas.length
 
   return (
     <>
       <div className='bg-white p-4 md:p-10 rounded-lg shadow-lg ml-10 mr-5 mt-2'>
-        <h1 className="font-bold text-3xl text-center">Nota: {Puntuacion}/100</h1>
-        <h2 className="font-bold text-center">{Consejo}</h2>
+        <h1 className="font-bold text-3xl text-center">Nota: {promedio}/100</h1>
+        <h2 className="font-bold text-center">{ResumenGeneral}</h2>
       </div>
 
     </>
